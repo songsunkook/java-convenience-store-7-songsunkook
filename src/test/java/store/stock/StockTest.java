4d2a4b5667f81836f0a24344b09b8747cb1c5beb
songsunkook <songsunkook@gmail.com>
2024-11-07 19:29:53 +0900

    test: 재고 수량을 고려하여 결제 가능 여부 확인

---
+++ src/test/java/store/stock/StockTest.java
@@ -0,0 +1,23 @@
+package store.stock;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class StockTest {
+
+    @Test
+    void 재고가_충분하면_구매가_정상적으로_진행된다() {
+        // name,price,quantity,promotion
+        Stock stock = new Stock("콜라", 1000, 10, null);
+        stock.buy(5);
+    }
+
+    @Test
+    void 재고가_부족하면_예외를_반환한다() {
+        Stock stock = new Stock("콜라", 1000, 10, null);
+        assertThatThrownBy(() -> stock.buy(15))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
