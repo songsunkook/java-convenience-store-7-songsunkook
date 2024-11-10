f3d70ffd3edb8b4aedab960a7bffaf5ea0d78602
songsunkook <songsunkook@gmail.com>
2024-11-10 22:01:18 +0900

    refactor: 정규표현식 적용

--- src/main/java/store/dto/OrderRequest.java
+++ src/main/java/store/dto/OrderRequest.java
@@ -1,17 +1,27 @@
 package store.dto;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import store.exception.argument.InvalidInputFormatException;
 
 public record OrderRequest(String name, int quantity) {
 
+    private static final String REGEX = "\\[(.*)-(.*)\\]";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+
     public static OrderRequest from(String input) {
-        // TODO: 정규식
+        Matcher matcher = PATTERN.matcher(input);
+        if (!matcher.matches()) {
+            throw new InvalidInputFormatException();
+        }
+        return new OrderRequest(matcher.group(1), toInteger(matcher.group(2)));
+    }
+
+    private static int toInteger(String content) {
         try {
-            String[] split = input.split("-");
-            String name = split[0].substring(1);
-            int count = Integer.parseInt(split[1].substring(0, split[1].length() - 1));
-            return new OrderRequest(name, count);
-        } catch (Exception e) {
+            return Integer.parseInt(content);
+        } catch (NumberFormatException e) {
             throw new InvalidInputFormatException();
         }
     }
