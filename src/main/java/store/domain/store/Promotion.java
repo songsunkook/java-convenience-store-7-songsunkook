66749859c30842e45e90633c78af141d386eff85
songsunkook <songsunkook@gmail.com>
2024-11-11 13:25:01 +0900

    fix: 프로모션 마지막날이 기간에 포함되도록 수정

--- src/main/java/store/domain/store/Promotion.java
+++ src/main/java/store/domain/store/Promotion.java
@@ -4,23 +4,25 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Promotion {
+
+    private static final int NEXT_DAY = 1;
+
     private final String name;
     private final int buy;
     private final int get;
-    private final LocalDate startDate;
-    private final LocalDate endDate;
+    private final LocalDateTime startDateTime;
+    private final LocalDateTime endDateTime;
 
     public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
         this.name = name;
         this.buy = buy;
         this.get = get;
-        this.startDate = startDate;
-        this.endDate = endDate;
+        startDateTime = startDate.atStartOfDay();
+        endDateTime = endDate.plusDays(NEXT_DAY).atStartOfDay();
     }
 
     public boolean inProgress(LocalDateTime now) {
-        // TODO: 종료일이 포함되는지 확인하기
-        return now.isAfter(startDate.atStartOfDay()) && now.isBefore(endDate.atStartOfDay());
+        return now.isAfter(startDateTime) && now.isBefore(endDateTime);
     }
 
     public int bonus(int buy) {
